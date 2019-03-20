87ae203b759f1f4240b5e4ae279cf2b4ddd664f0
kennypee <ekeneeze3@gmail.com>
2019-03-20 15:49:24 +0100

    Realtime features

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="vueapp" class="vue-app">
+    <kendo-chart :title-text="'Realtime Stockchart'"
+                 :legend-position="'bottom'"
+                 :series="series"
+                 :category-axis-categories="categories"
+                 :value-axis="axis"
+                 :theme="'sass'">
+    </kendo-chart>
+</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import '@progress/kendo-theme-default/dist/all.css'
 
+import { Chart } from '@progress/kendo-charts-vue-wrapper'
+import { setInterval } from 'timers';
+import io from "socket.io-client";
+var socket = io.connect("http://localhost:4000");
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Chart
+  },
+  mounted(){
+    this.getSeriesData()
+  },
+  created(){
+    this.getRealtimeData()
+  },
+  data() {
+    return {
+      series:[],
+        categories: ["Product1", "Product2", "Product3"],
+        axis: [
+          {
+            name: "stockrange",
+            labels: {
+                format: "{0}%"
+            }
+        },
+       
+        ]
+      }
+    },
+    methods:{
+    getSeriesData(fetchedData){
+      this.series = [{
+            name: 'Microsoft',
+            data: [this.getRandomChartValues(fetchedData), 
+                  this.getRandomChartValues(fetchedData), 
+                  this.getRandomChartValues(fetchedData)],
+            axis: "stockrange"
+        },
+        {
+            name: 'Google',
+            data: [this.getRandomChartValues(fetchedData), 
+                  this.getRandomChartValues(fetchedData), 
+                  this.getRandomChartValues(fetchedData)],
+            axis: "stockrange"
+        },
+        {
+            name: 'Sealuse',
+            data: [this.getRandomChartValues(fetchedData), 
+                  this.getRandomChartValues(fetchedData), 
+                  this.getRandomChartValues(fetchedData)],
+            axis: "stockrange"
+        }]
+     
+      },
+    getRealtimeData() {
+      socket.on("newdata", fetchedData => {
+        this.getSeriesData(fetchedData) 
+      })
+    },
+    getRandomChartValues(number){
+      return Math.floor(Math.random() * number) + 10
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {ChartInstaller } from '@progress/kendo-charts-vue-wrapper'
+Vue.use(ChartInstaller)
 
 Vue.config.productionTip = false
 
